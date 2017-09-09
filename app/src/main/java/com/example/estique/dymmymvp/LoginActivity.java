f173dd5940150f527dd7601a7282563224a4502a
EA Rashel <rashel320@gmail.com>
2017-09-09 17:36:21 +0600

    add activity

---
+++ app/src/main/java/com/example/estique/dymmymvp/LoginActivity.java
@@ -0,0 +1,17 @@
+package com.example.estique.dymmymvp;
+
+/**
+ * Created by estique on 9/9/17.
+ */
+
+public class LoginActivity extends BaseActivity implements LoginMvpView{
+    @Override
+    public void openMainActivity() {
+
+    }
+
+    @Override
+    public void onLoginButtonClick() {
+
+    }
+}
